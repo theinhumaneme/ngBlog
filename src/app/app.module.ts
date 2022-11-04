0b14ba6a5c7ba0feeb4d5fa60dcfadc4eef14bfa
Mudumby Kalyan <kalyan@pop-os.localdomain>
2022-11-04 16:33:29 +0530

    ngBlog init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
+import { LoggingComponent } from './logging/logging.component';
+import { PostComponent } from './blog/post/post.component';
+import { CommentComponent } from './blog/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostComponent,
+    CommentComponent,
+    BlogComponent,
+    LoggingComponent
   ],
   imports: [
     BrowserModule,
